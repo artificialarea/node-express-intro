77600c3e855960d2e88a850950a7aea82357ef0e
Sacha Sedriks <sachased@gmail.com>
2020-06-25 14:30:30 -0700

    assignment 1 done.

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const morgan = require('morgan');
+const express = require('express'); // https://expressjs.com/en/4x/api.html
+const morgan = require('morgan');   // https://www.npmjs.com/package/morgan
 
-// calling this function creates an Express app and gives us access to express objects (Application, Request, Response, Router) and their various methods like .use(), .get(), etc.
+// calling this function creates an Express app and gives us access to express objects (Application, Request, Response, Router) and their various methods like .use(), .get(), etc. 
 const app = express();
 
 // Morgan is middleware that requests pass through
@@ -13,7 +13,31 @@ app.get('/', (req, res) => {
   res.send(`Hello Express! \n src: 06-25_node-express-intro`);
 })
 
-// setting up (express) server to listen to a specific port, in this case 8000
+
+// ASSIGNMENT /////////////////////////////////////////////////
+
+// ASSIGNMENT 1 ///////////////////////////////////////////////
+app.get('/sum', (req, res) => {
+
+  const a = req.query.a;
+  const b = req.query.b;
+  const c = (parseInt(a) + parseInt(b)).toString();
+
+  if(!a) {
+    return res.status(400).send('Number a is undefined')
+  }
+  if(!b) {
+    return res.status(400).send('Number b is undefined')
+  }
+
+  const answer = `The sum of ${a} and ${b} is ${c}`
+  res.send(answer)
+})
+
+// ^^ ASSIGNMENT //////////////////////////////////////////////
+
+
+// setting up (express) server to listen to a specific port
 app.listen(8000, () => {
   console.log('Express server is listening on port 8000...')
 })
