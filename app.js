924c0e56a9e8264d4a234db49b2061d53e01ffcc
Sacha Sedriks <sachased@gmail.com>
2020-06-27 09:35:09 -0700

    removed /sum and /cipher route handlers from this 'playground' branch.

--- app.js
+++ app.js
@@ -9,63 +9,6 @@ app.get('/', (req, res) => {
   res.send('Hello Express!')
 })
 
-app.get('/sum', (req, res) => {
-
-  const a = Number(req.query.a);
-  const b = Number(req.query.b);
-
-  if(!a) { 
-    return res.status(400).send('Number a is required.')
-  }
-  if(!b) {
-    return res.status(400).send('Number b is required.')
-  }
-
-  const c = (a + b).toString();
-
-  const answer = `The sum of ${a} and ${b} is ${c}`;
-
-  res.send(answer);
-
-})
-
-app.get('/cipher', (req, res) => {
-
-  const text = req.query.text.toUpperCase();
-  const shift = Math.abs(Number(req.query.shift));
-
-  if (shift > 26) {
-    return res.send("Shift must be 26 or less.");
-  }
-  // const charArray = text.split("");
-  const asciiArr = [];
-
-  for(let i=0; i < text.length; i++) {
-    asciiArr.push(text[i].charCodeAt(0))
-  }
-
-  const shifted = asciiArr.map(i => {
-    if(i === 32) {
-      return i;
-    }
-
-    if((i + shift) > 90) {
-      return (i + shift - 91) + 65
-    }
-    return (i + shift);
-  })
-
-  const cipher = String.fromCharCode(...shifted);
-
-
-
-  // const asciiArr = charArray.map(i => {
-  //   charArray[i].charCodeAt(0);
-  // })
-
-  res.send([text, shift, asciiArr, shifted, cipher]);
-
-})
 
 // TEMP PLAYGROUND ////////////////////////
 
@@ -128,7 +71,7 @@ app.get('/grade', (req, res) => {
   if (numericMark >= 90) {
     return res.send('A');
   }
-  if (numericMark > 80) {
+  if (numericMark >= 80) {
     return res.send('B');
   }
   if (numericMark >= 70) {
